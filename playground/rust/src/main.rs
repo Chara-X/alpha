ac686afc42b6aebd25fe50153dec3d4bea54a16f
魏攀6092004007 <wei.pan2@zte.com.cn>
2024-12-20 17:35:23 +0800

    Up

--- playground/rust/src/main.rs
+++ playground/rust/src/main.rs
@@ -1,1 +1,10 @@
-fn main() {}
+static X: SomeStruct = SomeStruct { x: 1 };
+struct SomeStruct {
+    x: i32,
+}
+const FOO: SomeStruct = SomeStruct { x: 1 };
+const BAR: SomeStruct = FOO;
+fn main() {
+    // let a = X;
+    // let b = X;
+}
