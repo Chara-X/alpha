f70464311374264badcb7752c03f508875c0e0f0
魏攀6092004007 <wei.pan2@zte.com.cn>
2024-12-26 18:45:22 +0800

    Up

--- playground/go/main.go
+++ playground/go/main.go
@@ -1,24 +1,4 @@
 package main
 
-import (
-	"log"
-
-	"github.com/gizak/termui/v3"
-	"github.com/gizak/termui/v3/widgets"
-)
-
 func main() {
-	if err := termui.Init(); err != nil {
-		log.Fatalf("failed to initialize termui: %v", err)
-	}
-	defer termui.Close()
-	var p = widgets.NewParagraph()
-	p.Text = "Hello World!"
-	p.SetRect(0, 0, 25, 5)
-	termui.Render(p)
-	for e := range termui.PollEvents() {
-		if e.Type == termui.KeyboardEvent {
-			break
-		}
-	}
 }
