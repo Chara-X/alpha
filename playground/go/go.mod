22b1d9460ddf72b96aedb84c0344368876b7829c
Chara-X <1719552189@qq.com>
2024-12-31 05:11:59 +0800

    Up

--- playground/go/go.mod
+++ playground/go/go.mod
@@ -6,7 +6,7 @@ replace github.com/Chara-X/github => ../../../github
 
 require (
 	github.com/agiledragon/gomonkey/v2 v2.12.0
-	github.com/gizak/termui/v3 v3.1.0
+	github.com/playwright-community/playwright-go v0.4901.0
 	github.com/prometheus/client_golang v1.20.3
 	golang.org/x/crypto v0.28.0
 	k8s.io/api v0.31.1
@@ -19,14 +19,17 @@ require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
 	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
 	github.com/evanphx/json-patch v5.9.0+incompatible // indirect
 	github.com/evanphx/json-patch/v5 v5.9.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.3 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
 	github.com/go-openapi/jsonreference v0.20.2 // indirect
 	github.com/go-openapi/swag v0.22.4 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
@@ -39,12 +42,9 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/mattn/go-runewidth v0.0.2 // indirect
-	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/nsf/termbox-go v0.0.0-20190121233118-02980233997d // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.55.0 // indirect
--- playground/go/main.go
+++ playground/go/main.go
@@ -1,4 +1,154 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"regexp"
+
+	"github.com/playwright-community/playwright-go"
+)
+
 func main() {
+	ExamplePlaywright()
+}
+func ExampleHackNews() {
+	{
+		pw, err := playwright.Run()
+		if err != nil {
+			log.Fatalf("could not start playwright: %v", err)
+		}
+		browser, err := pw.Chromium.Launch()
+		if err != nil {
+			log.Fatalf("could not launch browser: %v", err)
+		}
+		page, err := browser.NewPage()
+		if err != nil {
+			log.Fatalf("could not create page: %v", err)
+		}
+		if _, err = page.Goto("https://news.ycombinator.com"); err != nil {
+			log.Fatalf("could not goto: %v", err)
+		}
+		entries, err := page.Locator(".athing").All()
+		if err != nil {
+			log.Fatalf("could not get entries: %v", err)
+		}
+		for i, entry := range entries {
+			title, err := entry.Locator("td.title > span > a").TextContent()
+			if err != nil {
+				log.Fatalf("could not get text content: %v", err)
+			}
+			fmt.Printf("%d: %s\n", i+1, title)
+		}
+		if err = browser.Close(); err != nil {
+			log.Fatalf("could not close browser: %v", err)
+		}
+		if err = pw.Stop(); err != nil {
+			log.Fatalf("could not stop Playwright: %v", err)
+		}
+	}
+}
+func ExamplePlaywright() {
+	// Start Playwright
+	pw, err := playwright.Run()
+	if err != nil {
+		log.Fatalf("could not start Playwright: %v", err)
+	}
+	defer pw.Stop()
+	// Launch a browser
+	browser, err := pw.Chromium.Launch(playwright.BrowserTypeLaunchOptions{
+		Headless: playwright.Bool(true),
+	})
+	if err != nil {
+		log.Fatalf("could not launch browser: %v", err)
+	}
+	defer browser.Close()
+	// Create a new page
+	page, err := browser.NewPage()
+	if err != nil {
+		log.Fatalf("could not create page: %v", err)
+	}
+	// Test 1: Check if the title contains "Playwright"
+	_, err = page.Goto("https://playwright.dev/")
+	if err != nil {
+		log.Fatalf("could not visit page: %v", err)
+	}
+	title, err := page.Title()
+	if err != nil {
+		log.Fatalf("could not get title: %v", err)
+	}
+	// Use regular expression to check the title
+	match, _ := regexp.MatchString("Playwright", title)
+	if match {
+		fmt.Println("Test 1 passed: Title contains 'Playwright'")
+	} else {
+		fmt.Println("Test 1 failed: Title does not contain 'Playwright'")
+	}
+	// Test 2: Click the 'Get started' link and check for heading "Installation"
+	_, err = page.Goto("https://playwright.dev/")
+	if err != nil {
+		log.Fatalf("could not visit page: %v", err)
+	}
+	// Click the 'Get started' link
+	// err = page.Locator("text=Get started").Click()
+	x := page.GetByRole("link", playwright.PageGetByRoleOptions{
+		Name: "Get started",
+	})
+	fmt.Println(x.InnerText())
+	x.Click()
+	if err != nil {
+		log.Fatalf("could not click on 'Get started' link: %v", err)
+	}
+	// Wait for the 'Installation' heading to be visible
+	// heading, err := page.Locator("text=Installation").IsVisible()
+	x = page.GetByRole("heading", playwright.PageGetByRoleOptions{
+		Name: "Installation",
+	})
+	fmt.Println(x.InnerText())
+	if err != nil {
+		log.Fatalf("could not check visibility of 'Installation' heading: %v", err)
+	}
+	var visible bool
+	playwright.NewPlaywrightAssertions().Locator(x).ToBeVisible(playwright.LocatorAssertionsToBeVisibleOptions{
+		Visible: &visible,
+	})
+	fmt.Println(x.AriaSnapshot())
+	x.Click()
+	x.Highlight()
+	x.SetChecked(true)
+	x.Uncheck()
+	if visible {
+		fmt.Println("Test 2 passed: 'Installation' heading is visible")
+	} else {
+		fmt.Println("Test 2 failed: 'Installation' heading is not visible")
+	}
+}
+func ExamplePakWheels() {
+	pw, _ := playwright.Run()
+	defer pw.Stop()
+	browser, _ := pw.Chromium.Launch(playwright.BrowserTypeLaunchOptions{
+		Headless: playwright.Bool(true),
+	})
+	defer browser.Close()
+	page, _ := browser.NewPage()
+	page.Goto("https://www.pakwheels.com/")
+	searchBox := page.Locator("input[name='home-query']")
+	btn := page.Locator("button[id='home-search-btn']")
+	searchBox.Fill("Toyota Corolla")
+	btn.Click()
+	if err := page.WaitForLoadState(); err != nil {
+		panic(err)
+	}
+	classifiedListings := page.Locator("li.classified-listing")
+	count, _ := classifiedListings.Count()
+	fmt.Println(count)
+	for i := 0; i < count; i++ {
+		listing := classifiedListings.Nth(i)
+		fmt.Println(listing.InnerText())
+		_, err := listing.Screenshot(playwright.LocatorScreenshotOptions{
+			Path: playwright.String(fmt.Sprintf("capture/listing-%d.png", i+1)),
+		})
+		if err != nil {
+			panic(err)
+		}
+	}
 }
