eeceda8be47362a2d27cd5ce049edc3d1cee6c7e
Chara-X <1719552189@qq.com>
2025-01-03 06:29:16 +0800

    Up

--- playground/typescript/main.js
+++ playground/typescript/main.js
@@ -1,18 +1,14 @@
 import { chromium, devices } from 'playwright';
 import assert from 'assert';
-
 (async () => {
     // Setup
     const browser = await chromium.launch();
     const context = await browser.newContext(devices['iPhone 11']);
     const page = await context.newPage();
-
     // The actual interesting bit
     await context.route('**.jpg', route => route.abort());
     await page.goto('https://example.com/');
-
-    assert(await page.title() === 'Example Domain'); // 👎 not a Web First assertion
-
+    assert(await page.title() === 'Example1 Domain'); // 👎 not a Web First assertion
     // Teardown
     await context.close();
     await browser.close();
