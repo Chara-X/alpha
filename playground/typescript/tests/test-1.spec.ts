f0c11bb3b7203c8c9f4da9535d5711ed83745a8c
Chara-X <1719552189@qq.com>
2025-01-07 05:26:37 +0800

    Up

--- playground/typescript/tests/test-1.spec.ts
+++ playground/typescript/tests/test-1.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  // Recording...
+  await page.goto('https://www.npmjs.com/package/create-playwright');
+  await page.getByPlaceholder('Search packages').click();
+  await page.getByText('create-playwright', { exact: true }).click();
 });
