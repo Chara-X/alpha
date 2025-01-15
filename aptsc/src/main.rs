3cec0716f73abadea6c31cf8eaf334fff80e4cd8
魏攀6092004007 <wei.pan2@zte.com.cn>
2025-01-15 18:18:44 +0800

    Up

--- aptsc/src/main.rs
+++ aptsc/src/main.rs
@@ -1,1 +1,20 @@
 fn main() {}
+
+pub mod msg;
+
+use reqwest::blocking;
+use serde;
+#[derive(open_api::OpenApi)]
+pub struct Client {
+    c: blocking::Client,
+    svr: String,
+}
+impl Client {
+    #[open_api(method = "POST", path = "/opapi/wsm/v1/apts/inspection")]
+    pub fn create_gpu_inspection_task(
+        &self,
+        req: CreateGpuInspectionTaskRequest,
+    ) -> reqwest::Result<CreateGpuInspectionTaskResponse> {
+        todo!()
+    }
+}
