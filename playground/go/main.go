482f441bc561513ffd7f921be8235198f2b236bd
Chara-X <1719552189@qq.com>
2025-01-21 05:22:57 +0800

    Up

--- playground/go/main.go
+++ playground/go/main.go
@@ -1,15 +1,32 @@
 package main
 
-import (
-	"embed"
+import "fmt"
+
+//go:generate stringer -type=User
+type User struct {
+	ID   int
+	Name string
+}
+type Pill int
+
+const (
+	Placebo Pill = iota
+	Aspirin
+	Ibuprofen
+	Paracetamol
+	Acetaminophen = Paracetamol
 )
 
-// content holds our static web server content.
-//
-//go:embed cmd/* example/* "main.go"
-var content embed.FS
-
 func main() {
+	fmt.Println(Aspirin)
 }
-func Add() {
+func Div(x, y int) int {
+	return x / y
+}
+func Try(f func()) (err error) {
+	defer func() {
+		err, _ = recover().(error)
+	}()
+	f()
+	return
 }
