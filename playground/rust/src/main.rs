a00fd55cb3fa4ee318dbda4fb166ad7023ba52e0
Chara-X <1719552189@qq.com>
2025-02-06 04:14:20 +0800

    Up

--- playground/rust/src/main.rs
+++ playground/rust/src/main.rs
@@ -1,5 +1,22 @@
-fn main() {}
-struct User {
-    id: i32,
-    name: String,
+fn main() {
+    let p = PeopleImpl {};
+    do_something(p);
+}
+fn do_something(u: impl serde::de::DeserializeOwned) {}
+trait People {
+    fn walk(&self);
+}
+trait User: People {}
+struct PeopleImpl {}
+impl People for PeopleImpl {
+    fn walk(&self) {}
+}
+impl User for PeopleImpl {}
+impl<'de> serde::Deserialize<'de> for PeopleImpl {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        todo!()
+    }
 }
