b47f3a392f8294d5c56f0c75c9ae047134d970d4
魏攀6092004007 <wei.pan2@zte.com.cn>
2025-02-06 19:16:41 +0800

    Up

--- playground/rust/src/main.rs
+++ playground/rust/src/main.rs
@@ -1,22 +1,18 @@
-fn main() {
-    let p = PeopleImpl {};
-    do_something(p);
-}
-fn do_something(u: impl serde::de::DeserializeOwned) {}
-trait People {
-    fn walk(&self);
-}
-trait User: People {}
-struct PeopleImpl {}
-impl People for PeopleImpl {
-    fn walk(&self) {}
-}
-impl User for PeopleImpl {}
-impl<'de> serde::Deserialize<'de> for PeopleImpl {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        todo!()
+use k8s_openapi::api::core::v1::Pod;
+use kube::{
+    api::{Api, ListParams, ResourceExt},
+    Client,
+};
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Infer the runtime environment and try to create a Kubernetes Client
+    let mut config = kube::Config::infer().await.unwrap();
+    config.proxy_url = None; // Disable proxy
+    let client = Client::try_from(config).unwrap();
+    // Read pods in the configured namespace into the typed interface from k8s-openapi
+    let pods: Api<Pod> = Api::default_namespaced(client);
+    for p in pods.list(&ListParams::default()).await? {
+        println!("found pod {}", p.name_any());
     }
+    Ok(())
 }
